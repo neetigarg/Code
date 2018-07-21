70195a5511d26dd9ae8f3b07c4a890ae9a5ac5e3
Neeti Garg <ng2talk98@gmail.com>
2018-07-21 15:18:52 +0530

    Add files via upload

---
+++ Eclipse workspace/first/src/org/first/program/First.java
@@ -0,0 +1,14 @@
+package org.first.program;
+import java.util.*;
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		List<String> s = new ArrayList<String>();
+		s.add("New proj");
+		s.add("second");
+		System.out.println(" "+s);
+	}
+
+}
